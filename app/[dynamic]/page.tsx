9ba76c0baba1845978ad39a0a934304a7b73e4de
Florian Lopez Plaza <contact@infinity-fx.com>
2023-11-22 19:48:25 +0100

    Initial commit

---
+++ app/[dynamic]/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import Params from "./params";
+
+/** Add your relevant code here for the issue to reproduce */
+export default function Home() {
+  return <>
+    <Link href="/">Testing</Link>
+
+    <Params />
+  </>;
+}
